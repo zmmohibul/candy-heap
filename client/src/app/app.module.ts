84a385c778f6261c65c30c53ceb6f672f43395b2
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-06 20:42:55 +0600

    Client - Properly Added NGX-Bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgxNavbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
